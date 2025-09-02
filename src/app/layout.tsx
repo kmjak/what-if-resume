30b984c2147dfd9f28f7f82a260869ea19f2a8dc
kmjak <tymkoron1221@icloud.com>
2025-09-03 01:51:54 +0900

    chore: Next.jsの雛形作成

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "となりの履歴書",
+    template: "%s | となりの履歴書",
+  },
+  description:
+    "これは、あなたの経歴をもとに、もしもの履歴書を生成するアプリです。",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>となりの履歴書</h1>;
+}

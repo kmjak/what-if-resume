8486c8a8634a248f4fe7d239c8bc6ee42f787822
kmjak <tymkoron1221@icloud.com>
2025-10-31 05:24:34 +0900

    feat: 履歴書のデフォルトの値を定義
    内容
    - 履歴書のデフォルトの値を定義
    - バレルファイルの実装

--- src/features/resume/constants/index.ts
+++ src/features/resume/constants/index.ts
@@ -1,1 +1,10 @@
 export { proficiencyLevels, proficiencyLevelsJapanese } from "./proficiency";
+export {
+  createDefaultCareer,
+  createDefaultCertification,
+  createDefaultHobby,
+  createDefaultLanguage,
+  createDefaultStrengths,
+  createDefaultValues,
+  createDefaultWeaknesses,
+} from "./resumeDefaultValues";

7163d61fd30aa238832e81ecc76da971b6d25b24
kmjak <tymkoron1221@icloud.com>
2025-10-31 05:10:14 +0900

    feat: カスタムフェッチの関数を実装
    内容
    - カスタムフェッチの実装

---
+++ src/shared/usecases/fetch.ts
@@ -0,0 +1,43 @@
+import { errorMessage } from "@/shared/utils/error";
+import { z } from "zod";
+
+type CustomFetchProps<T> = {
+  schemas: z.ZodType<T>;
+  url: string;
+  options: RequestInit;
+};
+
+/**
+ * カスタムフェッチ関数
+ * @param {object} props - 引数オブジェクト
+ * @param {z.ZodType<T>} props.schemas - Zodスキーマ
+ * @param {string} props.url - APIエンドポイントのURL
+ * @param {RequestInit} [props.options] - フェッチオプション
+ * @returns {Promise<T>} - パースされたデータ
+ * @throws エラーメッセージを表示し、エラーを投げる
+ */
+export async function customFetch<T>({
+  schemas,
+  url,
+  options = {},
+}: CustomFetchProps<T>): Promise<T> {
+  try {
+    const response = await fetch(url, {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        ...options.headers,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Status code: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return schemas.parse(result.data);
+  } catch (error) {
+    errorMessage({ title: "APIリクエスト中にエラーが発生しました。", error });
+    throw error;
+  }
+}

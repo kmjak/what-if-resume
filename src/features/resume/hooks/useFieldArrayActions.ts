6f1a2d9c4a19e1b0c03c12569d00e54489c5d5a1
kmjak <tymkoron1221@icloud.com>
2025-10-31 05:33:34 +0900

    feat: 履歴書とFieldのActionのカスタムフックを実装
    内容
    - 履歴書のフォーム用のカスタムフックを実装
    - Career用のアクションのカスタムフックを実装
    - Hobby用のアクションのカスタムフックを実装
    - Certification用のアクションのカスタムフックを実装
    - Language用のアクションのカスタムフックを実装
    - バレルファイルの実装

---
+++ src/features/resume/hooks/useFieldArrayActions.ts
@@ -0,0 +1,121 @@
+import {
+  createDefaultCareer,
+  createDefaultCertification,
+  createDefaultHobby,
+  createDefaultLanguage,
+} from "@/features/resume/constants";
+import { Career, Certification, Hobby, Language } from "@/features/resume/types/Resume";
+import { useCallback } from "react";
+
+type UseCareerActionsProps = {
+  append: (value: Career) => void;
+  remove: (index: number) => void;
+};
+
+type UseHobbyActionsProps = {
+  append: (value: Hobby) => void;
+  remove: (index: number) => void;
+};
+
+type UseCertificationActionsProps = {
+  append: (value: Certification) => void;
+  remove: (index: number) => void;
+};
+
+type UseLanguageActionsProps = {
+  append: (value: Language) => void;
+  remove: (index: number) => void;
+};
+
+type ReturnActions = {
+  handleAdd: () => void;
+  handleRemove: (index: number) => void;
+};
+
+/**
+ * Career用のアクション
+ * @param {object} props - 引数オブジェクト
+ * @param {function} props.append - フィールド配列に要素を追加する関数
+ * @param {function} props.remove - フィールド配列から要素を削除する関数
+ * @returns {object} - フィールド配列操作用のハンドラー関数群
+ */
+function useCareerActions({ append, remove }: UseCareerActionsProps): ReturnActions {
+  const handleAdd = useCallback(() => {
+    append(createDefaultCareer());
+  }, [append]);
+
+  const handleRemove = useCallback(
+    (index: number) => {
+      remove(index);
+    },
+    [remove]
+  );
+
+  return { handleAdd, handleRemove };
+}
+
+/**
+ * Hobby用のアクション
+ * @param {object} props - 引数オブジェクト
+ * @param {function} props.append - フィールド配列に要素を追加する関数
+ * @param {function} props.remove - フィールド配列から要素を削除する関数
+ * @returns {object} - フィールド配列操作用のハンドラー関数群
+ */
+function useHobbyActions({ append, remove }: UseHobbyActionsProps): ReturnActions {
+  const handleAdd = useCallback(() => {
+    append(createDefaultHobby());
+  }, [append]);
+
+  const handleRemove = useCallback(
+    (index: number) => {
+      remove(index);
+    },
+    [remove]
+  );
+
+  return { handleAdd, handleRemove };
+}
+
+/** * Certification用のアクション
+ * @param {object} props - 引数オブジェクト
+ * @param {function} props.append - フィールド配列に要素を追加する関数
+ * @param {function} props.remove - フィールド配列から要素を削除する関数
+ * @returns {object} - フィールド配列操作用のハンドラー関数群
+ */
+function useCertificationActions({ append, remove }: UseCertificationActionsProps): ReturnActions {
+  const handleAdd = useCallback(() => {
+    append(createDefaultCertification());
+  }, [append]);
+
+  const handleRemove = useCallback(
+    (index: number) => {
+      remove(index);
+    },
+    [remove]
+  );
+
+  return { handleAdd, handleRemove };
+}
+
+/** * Language用のアクション
+ * @param {object} props - 引数オブジェクト
+ * @param {function} props.append - フィールド配列に要素を追加する関数
+ * @param {function} props.remove - フィールド配列から要素を削除する関数
+ * @returns {object} - フィールド配列操作用のハンドラー関数群
+ */
+function useLanguageActions({ append, remove }: UseLanguageActionsProps): ReturnActions {
+  const handleAdd = useCallback(() => {
+    append(createDefaultLanguage());
+  }, [append]);
+
+  const handleRemove = useCallback(
+    (index: number) => {
+      remove(index);
+    },
+    [remove]
+  );
+
+  return { handleAdd, handleRemove };
+}
+
+export { useCareerActions, useCertificationActions, useHobbyActions, useLanguageActions };
